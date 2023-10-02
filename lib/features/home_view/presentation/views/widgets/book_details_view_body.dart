58d33a4a0b99e03fa5f021f9b414f4d48e51a67d
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-10-02 22:09:58 +0300

    add CustomBookItem to BookDetailsView

--- lib/features/home_view/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home_view/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:clean_arch_app/features/home_view/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_appbar_book_details_view.dart';
@@ -7,11 +8,16 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomBookDetailsAppBAr(),
+          const CustomBookDetailsAppBAr(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: const FeaturedBookItem(),
+          ),
         ],
       ),
     );
