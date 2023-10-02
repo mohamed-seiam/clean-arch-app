57328cb9d00b67c7c3daa5bab70a848f2b4e7c26
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-10-02 23:42:05 +0300

    create SimilarBooksListView

--- lib/features/home_view/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home_view/presentation/views/widgets/book_details_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:clean_arch_app/core/utilis/styles.dart';
 import 'package:clean_arch_app/features/home_view/presentation/views/widgets/book_rating.dart';
 import 'package:clean_arch_app/features/home_view/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:clean_arch_app/features/home_view/presentation/views/widgets/similar_books_listview.dart';
 import 'package:flutter/material.dart';
-
 import 'books_action.dart';
 import 'custom_appbar_book_details_view.dart';
 
@@ -12,27 +12,39 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBAr(),
-          Padding(
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: CustomBookDetailsAppBAr(),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Padding(
             padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const FeaturedBookItem(),
           ),
-          const SizedBox(
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: SizedBox(
             height: 43,
           ),
-          const Text(
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Text(
             'The Jungle Book',
             style: Styles.style30,
           ),
-          const SizedBox(
-            height: 8,
-          ),
-          Opacity(
-            opacity: .8,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Opacity(
+          opacity: .8,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
             child: Text(
               'Rudyard Kipling',
               style: Styles.style18.copyWith(
@@ -41,16 +53,49 @@ class BookDetailsViewBody extends StatelessWidget {
               ),
             ),
           ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: BookRating(
             mainAxisAlignment: MainAxisAlignment.center,
           ),
-         const SizedBox(height: 37,),
-         const BookAction(),
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 37,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: BookAction(),
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              'You can also like',
+              style: Styles.style14.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 24),
+          child: SimilarBooksListView(),
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+      ],
     );
   }
 }
---
+++ lib/features/home_view/presentation/views/widgets/similar_books_listview.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'featured_list_view_item.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .15,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 4.w),
+            child: const FeaturedBookItem(),
+          );
+        },
+      ),
+    );
+  }
+}
