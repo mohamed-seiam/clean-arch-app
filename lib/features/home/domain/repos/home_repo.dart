f12a794671fcaa677146781c81b811b78a742660
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-10-04 06:38:30 +0300

    add EitherClass

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,7 @@
+import '../../../../core/errors/failure.dart';
 import '../entites/book_entity.dart';
-
+import 'package:dartz/dartz.dart';
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure,List<BookEntity>>> fetchNewestBooks();
 }
