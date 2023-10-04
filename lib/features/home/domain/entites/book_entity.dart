141ec66180f4dbfce0886fca57d81ddb2f3b67df
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-10-04 06:05:11 +0300

    create BookEntity and Home repo

---
+++ lib/features/home/domain/entites/book_entity.dart
@@ -0,0 +1,10 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authName;
+  final num price;
+  final num rating;
+
+  BookEntity(this.image, this.title, this.authName, this.price, this.rating);
+
+}
