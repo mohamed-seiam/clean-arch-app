eb657f88311890102505ced98cb580def72667c5
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-10-08 21:50:40 +0300

    refactor to useCase class

--- lib/features/home/domain/use_cases/fetch_featured_books_use_cases.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_cases.dart
@@ -1,15 +1,20 @@
 import 'package:clean_arch_app/features/home/domain/repos/home_repo.dart';
+import 'package:clean_arch_app/features/home/domain/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
 import '../entites/book_entity.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>,NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks(){
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>>call([NoParam ? param]) async {
+
+    return await homeRepo.fetchFeaturedBooks();
   }
+
 }
+
