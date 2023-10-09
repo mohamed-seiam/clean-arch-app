8748813000fd9b0792edf3b9010f5c623d1d6bd8
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-10-09 20:51:31 +0300

    create homeLocal DataSource

---
+++ lib/features/home/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,23 @@
+import '../../domain/entites/book_entity.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntity> fetchFeaturedBooks();
+
+  List<BookEntity> fetchNewestBooks();
+}
+
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+
+}
