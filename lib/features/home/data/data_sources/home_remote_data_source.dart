a7a2d90e650f0c3a128eaac126de7f2c49571960
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-10-09 20:29:43 +0300

    implement remote fetchFeaturedBooks datasource

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:clean_arch_app/core/utilis/api_services.dart';
+import 'package:clean_arch_app/features/home/data/models/book_model.dart';
 import 'package:clean_arch_app/features/home/domain/entites/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -8,10 +10,22 @@ abstract class HomeRemoteDataSource {
 
 
   class HomeRemoteDataSourceImplementation extends HomeRemoteDataSource {
+   final ApiServices apiServices;
+
+  HomeRemoteDataSourceImplementation({required this.apiServices});
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+  var data = await  apiServices.get(endPoint:'volumes?Filtering=free-ebooks&q=programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books= [];
+    for(var bookMap in data['items']) {
+       books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
   }
 
   @override
@@ -19,5 +33,7 @@ abstract class HomeRemoteDataSource {
     // TODO: implement fetchNewestBooks
     throw UnimplementedError();
   }
+
+
 
   }
