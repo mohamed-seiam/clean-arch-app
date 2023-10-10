7faff188125ca8fd625e1c3936ff436cf2a367c4
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-10-10 05:09:16 +0300

    refactor homeRepo implementation

--- lib/features/home/data/repos/home_rep_implement.dart
+++ lib/features/home/data/repos/home_rep_implement.dart
@@ -15,12 +15,13 @@ class HomeRepoImplementation extends HomeRepo {
   @override
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
     try {
-      var cachedBooks = homeLocalDataSource.fetchFeaturedBooks();
-      if (cachedBooks.isNotEmpty) {
-        return right(cachedBooks);
+      List<BookEntity> books ;
+       books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      var booksList = await homeRemoteDataSource.fetchFeaturedBooks();
-      return right(booksList);
+       books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
     } catch (error) {
       return left(Failure());
     }
@@ -29,12 +30,13 @@ class HomeRepoImplementation extends HomeRepo {
   @override
   Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
     try {
-      var cachedBooks = homeLocalDataSource.fetchNewestBooks();
-      if (cachedBooks.isNotEmpty) {
-        return right(cachedBooks);
+      List<BookEntity> books ;
+       books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      var booksList = await homeRemoteDataSource.fetchNewestBooks();
-      return right(booksList);
+       books = await homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
     } catch (error) {
       return left(Failure());
     }
